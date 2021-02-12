23a419f2db87721c6ec0029087e3c6f90dd467b8
pypy <git@pypy.moe>
2021-02-13 08:25:55 +0900

    lint

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,29 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  });
-
-  win.loadFile('./assets/index.html');
-  win.webContents.openDevTools();
+    var mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    mainWindow.loadFile('./assets/index.html');
+    mainWindow.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+    createWindow();
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-})
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-})
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
