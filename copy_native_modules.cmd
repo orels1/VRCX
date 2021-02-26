4c3aafeade628f55cfa9e5ca66d5dc5074b82590
pypy <git@pypy.moe>
2021-02-27 02:22:10 +0900

    unnecessary

--- copy_native_modules.cmd
+++
@@ -1,4 +0,0 @@
-mkdir build
-copy /Y "node_modules\vrcx-native\bin\win32-x64-85\vrcx-native.node" build\vrcx_native.node"
-copy /Y "node_modules\better-sqlite3\bin\win32-x64-85\better-sqlite3.node" "build\better_sqlite3.node"
-pause
