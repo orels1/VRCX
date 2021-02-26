be4e8d8e5227abc0ca61bd276fba69375fee9994
pypy <git@pypy.moe>
2021-02-27 02:42:24 +0900

    APP_PATH, APP_PRELOAD_JS

--- src/js/constants.js
+++ src/js/constants.js
@@ -2,10 +2,14 @@ const path = require('path');
 const { app, nativeImage } = require('electron');
 
 var APP_NAME = 'VRCX';
-var APP_ICON_PATH = path.join(app.getAppPath(), 'assets/icon.ico');
+var APP_PATH = app.getAppPath();
+var APP_PRELOAD_JS = path.join(APP_PATH, 'assets/preload.js');
+var APP_ICON_PATH = path.join(APP_PATH, 'assets/icon.ico');
 var APP_ICON = nativeImage.createFromPath(APP_ICON_PATH);
 
 module.exports = {
     APP_NAME,
-    APP_ICON
+    APP_PATH,
+    APP_PRELOAD_JS,
+    APP_ICON,
 };
